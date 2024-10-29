a989ccc649e5e05848f9e0fca6183bd2b2b66524
1micr01 <elias11a.e@gmail.com>
2024-10-29 10:49:05 +0100

    Publish to mqtt broker

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,24 @@
 #include <WiFiNINA.h>
 #include <secrets.h>
 #include <SPI.h>
+#include <ArduinoMqttClient.h>
 
 char ssid[] = AP_SSID;
 char password[] = AP_PASSWORD;
 int status = WL_IDLE_STATUS;
 int led = LED_BUILTIN;
 
-WiFiSSLClient client;
+char broker[] = "test.mosquitto.org";
+int brokerPort = 8886;
+
+char topic[] = "arduino/simple";
+
+unsigned long previousMillis = 0;
+unsigned int interval = 1000;
+int count = 0;
+
+WiFiSSLClient wifiClient;
+MqttClient mqttClient(wifiClient);
 
 void printStatus()
 {
@@ -43,43 +54,43 @@ void setup()
 
   while (status != WL_CONNECTED){
     status = WiFi.begin(ssid, password);
-
     delay(10000);
   }
 
   printStatus();
   
-  if (client.connect("demosamples.azurewebsites.net", 443))
-  {
-    Serial.println("connected to server");
-
-    client.println("GET /StaticHTMLTest/Default.html HTTP/1.1");
-
-    client.println("Host: demosamples.azurewebsites.net");
-
-    client.println("Connection: close");
-
-    client.println();
+  if (!mqttClient.connect(broker, brokerPort)){
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+    while (true);
   }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
 }
 
 void loop()
 {
-  while (client.available())
+  mqttClient.poll();
+
+  unsigned long currentMillis = millis();
+
+  if ((currentMillis - previousMillis) >= interval)
   {
-    char c = client.read();
-    Serial.print(c);
-  }
-  
-  if (!client.connected())
-  {
+    previousMillis = currentMillis;
+
+    Serial.println("Sending to topic:");
+    Serial.println(topic);
+    Serial.println("Message:");
+    Serial.println("Mojn");
+    Serial.println(count);
     Serial.println();
 
-    Serial.println("disconnecting from server.");
+    mqttClient.beginMessage(topic);
+    mqttClient.print("Mojn");
+    mqttClient.endMessage();
 
-    client.stop();
-
-    while (true);
+    count++;
   }
-  
+    
 }
